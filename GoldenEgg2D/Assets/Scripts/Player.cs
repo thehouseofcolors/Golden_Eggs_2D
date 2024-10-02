96708f59747ca34de57b54419e89d150fff274ee
thehouseofcolors <148017890+thehouseofcolors@users.noreply.github.com>
2024-10-02 10:05:00 +0300

    .

--- GoldenEgg2D/Assets/Scripts/Player.cs
+++ GoldenEgg2D/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 touchPosition = Vector2.zero;
+
+        // Dokunma veya mouse pozisyonunu al
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        // Yalnýzca yatay eksende hareket
+        Vector2 newPosition = new Vector2(touchPosition.x, transform.position.y);
+
     }
 }
